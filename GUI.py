a4a431e0456890a9d0ec1f9cabf2a1e51fc2f330
Abhijeet-Katiyar <katiyar.abhijeet002@gmail.com>
2020-08-06 19:13:43 +0530

    added functionality to view sentences of each finding

--- GUI.py
+++ GUI.py
@@ -23,17 +23,33 @@ pathLabel=Label(inputFrame,text="Directory path...")
 pathLabel.grid(row=0,column=0)
 BrowseButton=Button(inputFrame,text="Select Folder",command=select)
 BrowseButton.grid(row=0,column=1)
+var=StringVar()
+
+def viewDetails(results,filename):
+    top=Toplevel()
+    #myLabel=Label(top,text=filename).pack()
+    for i in results[filename]:
+        s="page: "+str(i)+"\tSentence: "+results[filename][i]
+        myLabel=Label(top,text=s).pack()
+
 
 def Search(keyword):
     global outputLabel
+    global var
+    var=StringVar()
     results=getResults(root.filepath,keyword)
     for index,i in enumerate(results):
         filename="File Name: "+str(i)
         pagenum="pagenumbers: "+str(list(results[i].keys()))
         filename_outputLabel=Label(outputFrame,text=filename,anchor="w")
-        filename_outputLabel.grid(row=index,column=0,padx=5,pady=5,sticky="w")
+        filename_outputLabel.grid(row=index,column=0,padx=10,pady=10,sticky="w")
         pagenum_outputLable=Label(outputFrame,text=pagenum,anchor="w",wraplength=800,justify=LEFT)
-        pagenum_outputLable.grid(row=index,column=1,padx=5,pady=5,sticky="w")
+        pagenum_outputLable.grid(row=index,column=1,padx=10,pady=10,sticky="w")
+        #adding radio buttons
+        Radiobutton(outputFrame,variable=var,value=i).grid(row=index,column=2)
+    detailButton=Button(outputFrame,text="view Details",command= lambda: viewDetails(results,var.get()))
+    detailButton.grid(row=len(results),column=0)
+
 
 
 keywordEntry=Entry(inputFrame,width=50,borderwidth=5)
